11472900fdb6d87bd7584214308cb1d0e14a5b0c
igoqueen5 <igoqueen5@gmail.com>
2017-05-27 11:45:33 +0000

    push api data

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,4 +3,9 @@ def index
   @poems = Poem.all
   render json: @poems
 end
+
+def show
+  @poem = Poem.find(params[:id])
+  render json: @poem
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521122658) do
+ActiveRecord::Schema.define(version: 20170524225239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20170521122658) do
   end
 
   create_table "poems", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shows", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
